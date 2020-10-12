07fc63bceb4b595c7634f040c87a1bfe6a29aff9
Gonchar Alexis <alexgonchar00@gmail.com>
2020-10-12 19:02:12 +0300

    Added recieve message for server.

--- Lab3PSP/ServerApp/ServerApp.cpp
+++ Lab3PSP/ServerApp/ServerApp.cpp
@@ -1,1 +1,103 @@
-﻿
+﻿// Пример простого TCP-эхо-сервера
+
+#include <stdio.h>
+#pragma comment(lib, "ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h> // Wincosk2.h должен быть раньше windows!
+#include <windows.h>
+#include <locale.h>
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+#define MY_PORT 666 // Порт, который слушает сервер 666
+
+char buff[1024];
+
+int main()
+{
+    cout << "SERVER APPLICATION \n";
+
+    // Шаг 1 - Инициализация Библиотеки Сокетов
+    if (WSAStartup(0x0202, (WSADATA*)&buff[0]))
+    {
+        // Ошибка!
+        printf("Error WSAStartup %d\n", WSAGetLastError());
+        return -1;
+    }
+
+    // Шаг 2 - создание сокета
+    SOCKET mysocket;
+    // AF_INET - сокет Интернета
+    // SOCK_STREAM - потоковый сокет (с установкой соединения)
+    // 0 - по умолчанию выбирается TCP протокол
+    if ((mysocket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        // Ошибка!
+        printf("Error socket %d\n", WSAGetLastError());
+        WSACleanup(); // Деиницилизация библиотеки Winsock
+        return -1;
+    }
+
+    // Шаг 3 - связывание сокета с локальным адресом
+    sockaddr_in local_addr;
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(MY_PORT); // не забываем о сетевом порядке!!!
+    local_addr.sin_addr.s_addr = 0; // сервер принимает подключения
+                                    // на все свои IP-адреса
+
+    // вызываем bind для связывания
+    if (bind(mysocket, (sockaddr*)&local_addr, sizeof(local_addr)))
+    {
+        // Ошибка
+        printf("Error bind %d\n", WSAGetLastError());
+        closesocket(mysocket); // закрываем сокет!
+        WSACleanup();
+        return -1;
+    }
+
+    // Шаг 4 - ожидание подключений
+    // размер очереди - 0x100
+    if (listen(mysocket, 0x100))
+    {
+        // Ошибка
+        printf("Error listen %d\n", WSAGetLastError());
+        closesocket(mysocket);
+        WSACleanup();
+        return -1;
+    }
+
+    cout << "SERVER IS READY!\n";
+    cout << "Waiting for connections...\n";
+
+    // Шаг 5 - извлекаем сообщение из очереди
+    SOCKET client_socket; // сокет для клиента
+    sockaddr_in client_addr; // адрес клиента (заполняется системой)
+
+    // функции accept необходимо передать размер структуры
+    int client_addr_size = sizeof(client_addr);
+
+    // цикл извлечения запросов на подключение из очереди
+    while ((client_socket = accept(mysocket, (sockaddr*)&client_addr, \
+        & client_addr_size)))
+    {
+        char message[1024];
+        // пытаемся получить имя хоста
+        HOSTENT* hst;
+        hst = gethostbyaddr((char*)&client_addr.sin_addr.s_addr, 4, AF_INET);
+
+        // вывод сведений о клиенте
+        printf("+%s [%s] new connect!\n",
+            (hst) ? hst->h_name : "", inet_ntoa(client_addr.sin_addr));
+        
+        int bytes_recv;
+        bytes_recv = recv(client_socket, &message[0], 1024, 0);
+        message[bytes_recv] = 0;
+        cout << message;
+        printf("-disconnect\n");
+        // закрываем сокет
+        closesocket(client_socket);
+    }
+    return 0;
+}
