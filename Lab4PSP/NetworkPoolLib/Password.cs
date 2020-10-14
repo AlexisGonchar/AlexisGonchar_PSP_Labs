ae117325e95e7a93acc7059d1e2ef0a714219d82
Gonchar Alexis <alexgonchar00@gmail.com>
2020-10-14 12:57:29 +0300

    (lab4) Create Parssword class.

---
+++ Lab4PSP/NetworkPoolLib/Password.cs
@@ -0,0 +1,18 @@
+﻿namespace NetworkPoolLib
+{
+    public class Password
+    {
+        public string Pass { get; private set; }
+
+        public Password(string pass)
+        {
+            Pass = pass;
+        }
+
+        public static bool CheckValid()
+        {
+            bool result = false;
+            return result;
+        }
+    }
+}
