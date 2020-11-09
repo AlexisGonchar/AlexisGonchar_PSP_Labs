d21f838d3915d608b1c6c2fa771b715e3bac461e
Gonchar Alexis <alexgonchar00@gmail.com>
2020-11-09 10:32:36 +0300

    (lab5) Create server.

---
+++ Lab5PSP/Lab5PSP/Client.cs
@@ -0,0 +1,6 @@
+﻿namespace Lab5PSP
+{
+    class Client
+    {
+    }
+}
--- Lab5PSP/Lab5PSP/Server.cs
+++ Lab5PSP/Lab5PSP/Server.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace Lab5PSP
 {
     class Server
     {
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+
+        // Запуск сервера
+        public Server(int Port)
+        {
+            string s = IPAddress.Any.ToString();
+            Listener = new TcpListener(IPAddress.Any, Port); // Создаем "слушателя" для указанного порта
+            Listener.Start(); // Запускаем его
+
+            // В бесконечном цикле
+            while (true)
+            {
+                // Принимаем новых клиентов. После того, как клиент был принят, он передается в новый поток (ClientThread)
+                // с использованием пула потоков.
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), Listener.AcceptTcpClient());
+            }
+        }
     }
 }
